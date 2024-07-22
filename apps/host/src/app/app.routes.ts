c6b9ab1a705c3dd56d99b1ef7373d30ccbb70f97
Diego Ronan Kulian <dkulian@veepee.com>
2024-07-22 13:34:42 +0200

    add remote application

--- apps/host/src/app/app.routes.ts
+++ apps/host/src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'account-remote',
+    loadChildren: () =>
+      import('account-remote/Routes').then((m) => m.remoteRoutes),
+  },
+  {
     path: '',
     component: NxWelcomeComponent,
   },
