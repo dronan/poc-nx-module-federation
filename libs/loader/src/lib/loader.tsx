820ab8045c610095c01764f63b062e71c3a692f5
Diego Ronan Kulian <dkulian@veepee.com>
2024-07-22 15:38:17 +0200

    creating a react lib

---
+++ libs/loader/src/lib/loader.tsx
@@ -0,0 +1,11 @@
+import styles from './loader.module.scss';
+
+export function Loader() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Loader!</h1>
+    </div>
+  );
+}
+
+export default Loader;
